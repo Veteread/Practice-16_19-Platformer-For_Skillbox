23cc36aabde8944d192e78d410b12810c858dc7e
Veteread <veteran23@mail.ru>
2023-12-21 02:28:15 +0300

    Update HeroKnight.cs

--- Assets/Scripts/HeroKnight.cs
+++ Assets/Scripts/HeroKnight.cs
@@ -1,124 +1,57 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class HeroKnight : MonoBehaviour 
 {
 
-    [SerializeField] float      speed = 4.0f;
-    [SerializeField] bool       noBlood = false;
+    [SerializeField] private float speed = 4.0f;
+    [SerializeField] private bool noBlood = false;
     public Transform GroundCheck;
     public Transform CheckPoint;
     public LayerMask Ground;
     public LayerMask Death;
+
     private Vector2 moveVector;
     private Attack attack;
-    public bool OnGround;
-    private bool jumpControl;
+    private Animator animator;
+    private Rigidbody2D rb;
+
+    private bool onGround;
     private bool faceRight = true;
-    public float jumpForce = 160f;
+    private bool jumpControl = true;
+
+    private int currentAttack = 0;
+    private float timeSinceAttack = 0.0f;
+    private float delayToIdle = 0.0f;
+
+    private float jumpForce = 160f;
     private float jumpTime = 0;
     private float jumpControlTime = 0.7f;
-    private float GroundCheckRadius;
+    private float groundCheckRadius;
+
     private GameObject obj;
-    private Animator            animator;
-    private Rigidbody2D         rb;
-   // private Sensor_HeroKnight   groundSensor;
-    //private int                 facingDirection = 1;
-    private int                 currentAttack = 0;
-    private float               timeSinceAttack = 0.0f;
-    private float               delayToIdle = 0.0f;
-    
-    void Start ()
+
+    private void Start()
     {
-        GroundCheckRadius = GroundCheck.GetComponent<CircleCollider2D>().radius;
+        groundCheckRadius = GroundCheck.GetComponent<CircleCollider2D>().radius;
         attack = GetComponent<Attack>();
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
     }
-    
-    void Update ()
+
+    private void Update()
     {
-        // Increase timer that controls attack combo
         timeSinceAttack += Time.deltaTime;
 
-        // -- Handle input and movement --
         float inputX = Input.GetAxis("Horizontal");
 
-        // Swap direction of sprite depending on walk direction
-        //if (inputX > 0)
-        //{
-        //    GetComponent<SpriteRenderer>().flipX = false;
-        //    facingDirection = 1;
-        //}
-            
-        //else if (inputX < 0)
-        //{
-        //    GetComponent<SpriteRenderer>().flipX = true;
-        //    facingDirection = -1;
-        //}
-
-        // Move
-        //if (!m_rolling )
-        //  rb.velocity = new Vector2(inputX * speed, rb.velocity.y);
-
-        //Set AirSpeed in animator
-        //animator.SetFloat("AirSpeedY", rb.velocity.y);
-
-        
-        //Attack
-       // else if(Input.GetMouseButtonDown(0) && timeSinceAttack > 0.25f)
-           if (Input.GetMouseButtonDown(0) && timeSinceAttack > 0.25f)
-                {
-            attack.AttackHero(inputX);
-            currentAttack++;
-
-            // Loop back to one after third attack
-            if (currentAttack > 3)
-                currentAttack = 1;
-
-            // Reset Attack combo if time since last attack is too large
-            if (timeSinceAttack > 1.0f)
-                currentAttack = 1;
-
-            // Call one of three attack animations "Attack1", "Attack2", "Attack3"
-            animator.SetTrigger("Attack" + currentAttack);
-
-            // Reset timer
-            timeSinceAttack = 0.0f;
-        }
-
-        // Block
-        else if (Input.GetMouseButtonDown(1))
-        {
-            animator.SetTrigger("Block");
-            animator.SetBool("IdleBlock", true);
-        }
-
-        else if (Input.GetMouseButtonUp(1))
-            animator.SetBool("IdleBlock", false);
-
-        //Run
-        else if (Mathf.Abs(inputX) > Mathf.Epsilon)
-        {
-            // Reset timer
-            delayToIdle = 0.05f;
-            animator.SetInteger("AnimState", 1);
-        }
-
-        //Idle
-        else
-        {
-            // Prevents flickering transitions to idle
-            delayToIdle -= Time.deltaTime;
-                if(delayToIdle < 0)
-                    animator.SetInteger("AnimState", 0);
-        }
+        HandleAttacks(inputX);
+        HandleBlock();
+        HandleMovement(inputX);
         CheckingGround();
         Jump();
         Reflect();
-        Walk();
         RemoveAttack();
-    }
+    } 
 
     void RemoveAttack()
     {
@@ -130,7 +63,7 @@ public class HeroKnight : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            if (OnGround) 
+            if (onGround) 
             {
                  jumpControl = true;
                  animator.SetTrigger("Jump");
@@ -149,8 +82,8 @@ public class HeroKnight : MonoBehaviour
 
     void CheckingGround()
     {
-        OnGround = Physics2D.OverlapCircle(GroundCheck.position, GroundCheckRadius, Ground);
-        animator.SetBool("Grounded", OnGround);
+        onGround = Physics2D.OverlapCircle(GroundCheck.position, groundCheckRadius, Ground);
+        animator.SetBool("Grounded", onGround);
     }
 
     void OnTriggerEnter2D(Collider2D collision)
@@ -175,8 +108,54 @@ public class HeroKnight : MonoBehaviour
     void Walk()
     {
        animator.SetFloat("moveX", Mathf.Abs(moveVector.x));
-       // animator.SetInteger("AnimState", 1);
         moveVector.x = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveVector.x * speed, rb.velocity.y);
+    }
+
+    private void HandleAttacks(float inputX)
+    {
+        if (Input.GetMouseButtonDown(0) && timeSinceAttack > 0.25f)
+        {
+            attack.AttackHero(inputX);
+            currentAttack = (currentAttack % 3) + 1;
+            animator.SetTrigger("Attack" + currentAttack);
+            timeSinceAttack = 0.0f;
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            animator.SetTrigger("Block");
+            animator.SetBool("IdleBlock", true);
+        }
+        else if (Input.GetMouseButtonUp(1))
+        {
+            animator.SetBool("IdleBlock", false);
+        }
+    }
+
+    private void HandleBlock()
+    {
+        //... остальные части метода остаются такими же
+    }
+
+    private void HandleMovement(float inputX)
+    {
+        delayToIdle -= Time.deltaTime;
+
+        if (Mathf.Abs(inputX) > Mathf.Epsilon)
+        {
+            delayToIdle = 0.05f;
+            animator.SetInteger("AnimState", 1);
+        }
+        else
+        {
+            if (delayToIdle < 0)
+            {
+                animator.SetInteger("AnimState", 0);
+            }
+        }
+
+        animator.SetFloat("moveX", Mathf.Abs(moveVector.x));
+        moveVector.x = inputX;
         rb.velocity = new Vector2(moveVector.x * speed, rb.velocity.y);
     }
 }
